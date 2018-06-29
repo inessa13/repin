17ac7a6b38639c24d4e63222b7be264a7542fcda
David Djanyan <david.djanyan@exness.com>
2018-06-29 13:54:15 +0300

    Bump version

--- repin/__init__.py
+++ repin/__init__.py
@@ -2,6 +2,6 @@ from collections import namedtuple
 
 VersionInfo = namedtuple('VersionInfo', ('major', 'minor', 'micro'))
 
-VERSION = VersionInfo(0, 1, 0)
+VERSION = VersionInfo(0, 2, 0)
 
 __version__ = '{0.major}.{0.minor}.{0.micro}'.format(VERSION)
