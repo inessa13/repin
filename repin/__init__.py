b18f1b977f23f3720541b3d8a5781b736342f451
David Jhanyan <david.jhanyan@gatech.global>
2019-10-02 08:45:44 +0300

    Bump version to 0.3.0

--- repin/__init__.py
+++ repin/__init__.py
@@ -1,7 +1,1 @@
-from collections import namedtuple
-
-VersionInfo = namedtuple('VersionInfo', ('major', 'minor', 'micro'))
-
-VERSION = VersionInfo(0, 2, 3)
-
-__version__ = '{0.major}.{0.minor}.{0.micro}'.format(VERSION)
+__version__ = '0.3.0'
